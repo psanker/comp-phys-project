bcb3b2820a7dd1e8143ca779d7b997932f118490
psanker <patricksanker@gmail.com>
2017-11-08 21:59:55 -0500

    Changed all plotting to be done with imshow
    ∆ Axes now show correct scaling!

--- src/project.py
+++ src/project.py
@@ -4,6 +4,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from scipy.fftpack import fftshift, fftfreq
+
 from .simplepf import SimplePupilFunction
 from .simplegausspf import DirtySimplePupilFunction
 from .cassegrainpf import CassegrainPupilFunction
@@ -15,11 +17,11 @@ from .gaussianrndf import GaussianRandomField
 PI     = np.pi
 TWO_PI = 2.*PI
 
-pupil  = SimplePupilFunction(diameter=1, samples=256, padscale=1.)
-dirty  = DirtySimplePupilFunction(diameter=50, samples=256, padscale=1.)
-caspup = CassegrainPupilFunction(diameter=50, b=20, samples=256, padscale=1.)
-dcaspf = DirtyCassegrainPupilFunction(diameter=50, b=20, samples=256, padscale=1.)
-square = SquarePupilFunction(diameter=50, samples=256, padscale=1.)
+pupil  = SimplePupilFunction(diameter=6., samples=128, padscale=1.1)
+dirty  = DirtySimplePupilFunction(diameter=50, samples=256, padscale=1.1)
+caspup = CassegrainPupilFunction(diameter=6.5, b=2., samples=256, padscale=1.1)
+dcaspf = DirtyCassegrainPupilFunction(diameter=50, b=20, samples=256, padscale=1.1)
+square = SquarePupilFunction(diameter=50, samples=256, padscale=1.1)
 gauss  = GaussianRandomField(pupil, lambda s : np.where(s > 1e-15, (1. / s)**(3.0), 0.))
 
 #### Memory management ####
@@ -39,35 +41,47 @@ def terminate():
     global dcaspf
     dcaspf = None
 
-
 #### Drawing logic ####
-def render_pupil(pupilFunc, k = TWO_PI):
+def render_pupil(pupilFunc, k = TWO_PI, color=None):
     '''
     pupilFunc: The complex Pupil Function to analyze
     k: Wavenumber of light (2π / λ)
     '''
-    X, Y = pupilFunc.configurationMesh()
+    s     = pupilFunc.padscale * pupilFunc.diameter
+    s_map = [-s, s, s, -s]
 
     fig, ax = plt.subplots()
-    ax.contourf(X, Y, pupilFunc.render(k, filtering=True))
+
+    if color is not None:
+        ax.imshow(pupilFunc.render(k, filtering=True).real, cmap=plt.get_cmap(color), extent=s_map)
+    else:
+        ax.imshow(pupilFunc.render(k, filtering=True).real, extent=s_map)
+
     ax.set_aspect('equal')
 
-def render_psf(pupilFunc, k = TWO_PI):
+def render_psf(pupilFunc, k = TWO_PI, color=None):
     '''
     pupilFunc: The complex Pupil Function to analyze
     k: Wavenumber of light (2π / λ)
     '''
-    # Generate PSF
-    psf = pupilFunc.psf(filtering=True)
+    # PSF
+    psf = pupilFunc.psf(filtering=True)  # Generate PSF
+    psf = psf / np.amax(psf)             # Rescale output so max value is 1. (luminance)
+    psf = np.where(psf > 1e-15, psf, 0.) # Filter very low values
 
-    # Rescale output so max value is 1. (luminance)
-    psf = psf / np.amax(psf)
+    # Relevant k's
+    spacing = 1. / (2. * pupilFunc.nyqfreq())
+    k       = fftshift(fftfreq(pupilFunc.samples, d=spacing))
+    k_map   = [k[0], k[-1], k[-1], k[0]]
 
-    # Filter very low values
-    psf = np.where(psf > 1e-15, psf, 0.)
-
+    # Draw
     fig, ax = plt.subplots()
-    ax.imshow(psf)
+
+    if color is not None:
+        ax.imshow(psf, cmap=plt.get_cmap(color), extent=k_map)
+    else:
+        ax.imshow(psf, extent=k_map)
+
     ax.set_aspect('equal')
     ax.set_xlabel('$k_x$')
     ax.set_ylabel('$k_y$')
@@ -84,7 +98,7 @@ def plot_squarepupil():
     render_pupil(square)
 
 def plot_gsimplepupil():
-    render_pupil(dirty, k = TWO_PI / 20e-2)
+    render_pupil(dirty, k = TWO_PI / 20e-2, color='gray')
 
 # PSFs
 def plot_simplepsf():
