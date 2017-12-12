5c6c4964b09b1168b2db5f2426b6f4538917f75f
psanker <patricksanker@gmail.com>
2017-12-12 15:00:28 -0500

    hey i think this might do it

--- src/gaussianrndf.py
+++ src/gaussianrndf.py
@@ -9,6 +9,10 @@ import sys
 PI     = np.pi
 TWO_PI = 2. * PI
 
+import numpy as np
+
+TWO_PI = 2. * np.pi
+
 class GaussianRandomField(object):
     '''
     Returns a gaussian random field given a function P(k)
@@ -26,7 +30,26 @@ class GaussianRandomField(object):
         self.KX, self.KY = np.meshgrid(kx, kx, indexing='xy')
 
     def Pk(self, kx, ky):
-        return np.where(kx**2. + ky**2. > 0., (self.Pk2((kx**2. + ky**2.)**0.5))**0.5, 0.0)
+        # http://community.dur.ac.uk/james.osborn/thesis/thesisse3.html
+        l_min = 1e-3  # meters
+        l_max = 1e2   # meters
+        r0    = 20e-2 # meters
+
+        k_max = TWO_PI / l_min
+        k_min = TWO_PI / l_max
+
+        k2 = kx**2. + ky**2.
+
+        k2 = np.where(k2 < k_min**2, 0., k2)
+        k2 = np.where(k2 > k_max**2, 0., k2)
+
+        a  = 0.023
+
+        num = (np.abs(k2 + k_max**2))**(-11./6.)
+        dem = r0**(5./3.)
+        expfac = np.exp(-1. * (k2 / k_min**2))
+
+        return (num / dem) * expfac
 
     def randomfield(self, diameter):
         r0 = 20e-2 # coherence
@@ -34,7 +57,7 @@ class GaussianRandomField(object):
 
         phase = a * (diameter / r0)**(5/6)
 
-        noise = fftshift(fft2(np.random.normal(size = (self.samples, self.samples), scale=phase)))
+        noise = fftshift(fft2(np.random.normal(size=(self.samples, self.samples), scale=phase)))
         amplitude = self.Pk(self.KX, self.KY)
         
         amplitude = np.where(amplitude > 1e-15, amplitude, 0.)
