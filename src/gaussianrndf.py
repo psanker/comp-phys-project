9ffcb60958db576096495de2d1deacaee2e12e78
psanker <patricksanker@gmail.com>
2017-12-16 22:07:40 -0500

    IT WORKS

--- src/gaussianrndf.py
+++ src/gaussianrndf.py
@@ -20,13 +20,6 @@ class GaussianRandomField(object):
         kx               = np.arange(-self.samples/2, self.samples/2) # Both kx and ky are same
         self.KX, self.KY = np.meshgrid(kx, kx, indexing='xy')
 
-    def fftIndgen(self, n):
-        a = np.arange(0, n//2)
-        b = np.arange(1, n//2+1)
-
-        b = [-i for i in b[::-1]]
-        return a + b
-
     def Pk(self, kx, ky):
         # http://community.dur.ac.uk/james.osborn/thesis/thesisse3.html
         l_min = 1e-3  # meters
@@ -49,13 +42,13 @@ class GaussianRandomField(object):
 
         return a * (num / dem) * expfac
 
-    def kolmogorov_Pk(self, kx, ky):
-        a  = 0.0023
-        r0 = 20e-2 # meters
-
-        k = np.sqrt(kx**2 + ky**2)
-
-        return a * (r0**(-5./3.)) * (k**(-11./3.))
+    # def kolmogorov_Pk(self, kx, ky):
+    #    a  = 0.0023
+    #    r0 = 20e-2 # meters
+    #
+    #    k = np.sqrt(kx**2 + ky**2)
+    #
+    #    return a * (r0**(-5./3.)) * (k**(-11./3.))
 
     def randomfield(self):
         noise = fft2(np.random.normal(size=(self.samples, self.samples)))
@@ -65,15 +58,4 @@ class GaussianRandomField(object):
 
         amplitude = np.sqrt(pks * pk)
         
-        return fftshift(ifft2(amplitude))
-
-    def randomfield2(self):
-        noise = fft2(np.random.normal(size=(self.samples, self.samples)))
-        amplitude = np.zeros((self.samples, self.samples))
-
-        for i, kx in enumerate(self.fftIndgen(self.samples)):
-            for j, ky in enumerate(self.fftIndgen(self.samples)):           
-                amplitude[i, j] = self.Pk(kx, ky)
-                amplitude[i, j] = np.sqrt(np.conj(amplitude[i, j]) * amplitude[i, j])
-
-        return fftshift(ifft2(noise))
+        return fftshift(ifft2(noise * amplitude))
